47e5d985a98735e72e601838c74a972c5cd735d3
John Dunning <fw@johndunning.com>
2012-05-21 09:55:50 -0700

    Moved to do list to external file.

--- trace.js
+++ trace.js
@@ -37,81 +37,10 @@
 
 	See the readme for more information about the parameters that can be
 	passed to trace().
-
-	
-	To do:
-		- add try/catch around eval and alert the error?
-
-		- should probably pass log in as a parameter to the module 
-			would also need a string to supply the name for the function 
-			or possibly store a reference to it, then add it to the scope facade
-			if a function has a local log var, this will break
-
-		- make sure nested calls and calls to a function from one scope that
-			calls a function in another work
-
-		- dump the call stack when entering the function being traced?
-
-		- maybe use watch() to track the watched variables and log only when
-			they change
-			would calling watch() on the facade object work?
-
-		- maybe have an option to wrap everything in a try/catch and only log
-			on exceptions
-			return 'try {' + inWholeLine + '} catch (e) { log("' + functionName + 
-				'ERROR: ' + inStatement + '"); throw(e); }\n' + logCall;
-			problem is, we'd have to understand the block structure of the code
-			to not wrap try/catch around just the opening of a for loop, say
-
-		- make it work with module calls
-			function.toString sometimes puts all the statements on one line, 
-			so the regex doesn't match anything 
-		
-		- put watched vars on one line? 
-			gets tricky when you have tests and don't want to show anything
-			if the test doesn't match 
-
-	Done:
-		- make sure recursing via arguments.callee works 
-
-		- arguments.callee doesn't work, as it's pointing at the anonymous
-			function in the eval, not the original caller
-			also wouldn't work to set a property on callee, or to recurse
-
-		- treat watch expressions as assertions, show only when false
-
-		- a property on a function shadows a var in the parent scope of the same name
-			that's because we were calling walkScopeChain starting from the 
-				calling function itself, which added the properties on the 
-				function to the facade
-			need to start from caller.__parent__
-
-		- accessing the arguments var doesn't work
-
-		- show the whitespace of the statements in the log calls?
-			could make the code look more like the original function
-
-		- handle this correctly
-			may have to pass it to trace
-			return trace(this);
-			may need to pass this into anonymous functions in the eval
-
-		- try tracing a recursive function
-
-		- make sure returning a function from a function and then tracing the
-			returned one has the right scope 
-
-		- escape quotation marks 
-
-		- put logs after if and for/while loops
-
-		- capture the return value and display it
-
-		- call it trace
-
 */
 
 
+// ===========================================================================
 try { (function() {
 	if (typeof log != "function") {
 		alert("Before trace() can be called, the Fireworks Console extension must be installed and the panel must be open.  The extension can be downloaded from here:\n\n" + 
